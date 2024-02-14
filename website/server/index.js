23a30b3bcbe5de7fe70c5fd2bbd2e3465a47718b
fehervaryy <martindmhe@gmail.com>
2024-02-14 02:55:00 -0500

    added auth0 user authentication

--- website/server/index.js
+++ website/server/index.js
@@ -1,8 +1,23 @@
 const OpenAI = require('openai');
 const express = require('express');
 
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://127.0.0.1:3000',
+  clientID: 'HEzUXt66pZ04QPkb7XbUkZguPwdWeV6N',
+  issuerBaseURL: 'https://dev-irin453gbbxioje0.us.auth0.com'
+};
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(auth(config));
 
 const openai = new OpenAI({
     apiKey: "REMOVED API KEY FOR COMMIT"
@@ -20,9 +35,22 @@ async function textComplete(prompt) {
 
 app.use(express.json());
 
+//routes
+
+
+//auth0 routes
 app.get('/', (req, res) => {
-    console.log("Hi")
-})
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    console.log(JSON.stringify(req.oidc.user));
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
+// app.get('/', (req, res) => {
+//     console.log("Hi")
+// })
 
 let testPrompt = "What chatgpt model am i using?"
 
