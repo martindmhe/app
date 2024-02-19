daa25408a03e2057a5d709c9e9df9cb735976189
fehervaryy <martindmhe@gmail.com>
2024-02-18 19:16:26 -0500

    added aws user authentication

--- website/src/App.js
+++ website/src/App.js
@@ -8,21 +8,32 @@ import Sidebar from "./components/Sidebar";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 
-export default function App() {
+import { Amplify } from "aws-amplify";
+import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css"
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default withAuthenticator(function App() {
   return (
     <>
       <Router>
-        <Sidebar />
-        <Routes>
-          <Route path="/" exact element={<Home />}></Route>
-          <Route path="/help" exact element={<Help />}></Route>
-          <Route path="/register" exact element={<Register />}></Route>
-          <Route path="/login" exact element={<Login />}></Route>
 
-          <Route path="/generate" exact element={<Generate />}></Route>
-          <Route path="/upload" exact element={<Upload />}></Route>
-        </Routes>
-      </Router>
+          <Sidebar />
+          <Authenticator>
+            <Routes>
+              <Route path="/" exact element={<Home />}></Route>
+              <Route path="/help" exact element={<Help />}></Route>
+              <Route path="/register" exact element={<Register />}></Route>
+              <Route path="/login" exact element={<Login />}></Route>
+
+              <Route path="/generate" exact element={<Generate />}></Route>
+              <Route path="/upload" exact element={<Upload />}></Route>
+            </Routes>
+          </Authenticator>
+          
+        </Router>
     </>
   );
-}
+})
